f22ed6cd1712c30e6b40fbc490d7ce4f9233c8f6
Mike Hatch <mhatch@mhatch-ltm1.internal.salesforce.com>
2020-05-16 15:39:12 -0500

    added express and basic pages

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var neo4j = require('neo4j-driver');
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
 
 var graphenedbURL = process.env.GRAPHENEDB_BOLT_URL;
 var graphenedbUser = process.env.GRAPHENEDB_BOLT_USER;
@@ -12,15 +15,20 @@ var driver = neo4j.driver(
 
 var session = driver.session();
 
+express()
+.use(express.static(path.join(__dirname, 'public')))
+.set('views', path.join(__dirname, 'views'))
+.get('/', (req,res) => res.render('pages/index'))
+.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-session
-    .run("CREATE (n:Person {name: 'Bob'}) RETURN n.name")
-    .then(function(result) {
-        result.records.forEach(function(record) {
-            console.log(record)
-        });
-        session.close();
-    })
-    .catch(function(error) {
-        console.log(error);
-    });
+// session
+//     .run("CREATE (n:Person {name: 'Bob'}) RETURN n.name")
+//     .then(function(result) {
+//         result.records.forEach(function(record) {
+//             console.log(record)
+//         });
+//         session.close();
+//     })
+//     .catch(function(error) {
+//         console.log(error);
+//     });
