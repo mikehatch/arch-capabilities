6ba8bfc94740f0b5c6ab35b6b376b84b85ef805b
Mike Hatch <mhatch@mhatch-ltm1.internal.salesforce.com>
2020-05-30 15:10:25 -0500

    testing neo4j-driver

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var neo4j = require('neo4j-driver');
+//var neo4jGraphjs = require('./neo4jGraph.js');
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
@@ -9,26 +10,36 @@ var graphenedbPass = process.env.GRAPHENEDB_BOLT_PASSWORD;
 var driver = neo4j.driver(
     graphenedbURL, 
     neo4j.auth.basic(graphenedbUser, graphenedbPass),
-    { encrypted : true}
+    { encrypted : "ENCRYPTION_ON"}
     );
+//console.log(process.env.GRAPHENEDB_BOLT_URL);
+
 
 //playing with popoto - doesn't use bolt driver
 //var session = driver.session();
 
 express()
 .use(express.static(path.join(__dirname, '/')))
+//.use('/graph', neo4jGraphjs)
 .set('views', path.join(__dirname, 'views'))
+.set('view engine', 'ejs')
 .get('/', (req,res) => res.render('pages/index'))
 .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-// session
-//     .run("CREATE (n:Person {name: 'Bob'}) RETURN n.name")
-//     .then(function(result) {
-//         result.records.forEach(function(record) {
-//             console.log(record)
-//         });
-//         session.close();
-//     })
-//     .catch(function(error) {
-//         console.log(error);
-//     });
+var session = driver.session();
+//console.log(session);
+session.run('MATCH (n:Domain) return n.name')
+.subscribe({
+    onKeys: keys => {
+        console.log(keys)
+      },
+      onNext: record => {
+        console.log(record.get('name'))
+      },
+      onCompleted: () => {
+        session.close() // returns a Promise
+      },
+      onError: error => {
+        console.log(error)
+      }
+})
